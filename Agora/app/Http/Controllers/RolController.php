c9a0f6dd1486bb0c40165ec3072ab97d0d28ecc7
Elena Umanzor <elenuco36@gmail.com>
2023-04-09 15:14:47 -0700

    feat(I add new functions for rol module and user module): :zap: The next move is work in  the api
    2 modules develop rol anr user
    
    add select, insert and update rol the same way for user  module. is pending one bug.

--- Agora/app/Http/Controllers/RolController.php
+++ Agora/app/Http/Controllers/RolController.php
@@ -5,12 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use  DateTime;
 use App\Models\Rol;
+use App\Http\Controllers\ response()->download($pathToFile, $name, $headers);
 
 class RolController extends Controller
 {
-    public function listarRol(Request $request)
+    public function listarRol(Request $Request)
     {
         $rol= Rol::select('id', 'tipo_rol')->get();
         return response()->json($rol);
+    }
+    public function obtenerRol(Request $Request, $id)
+    {
+        $rol= Rol::where('id','=',$id)->first();
+        return response()->json($rol);
+    }
+    public function IngresarRol(Request $Request)
+    {
+        $data= array(
+            'tipo_rol'=>$Request->$tipo_rol
+        );
+        $nuevo_rol= new Rol($data);
+        $nuevo_rol->save();
+        return response()->json($nuevo_rol);
     }
 }
--- Agora/app/Http/Controllers/UsuarioController.php
+++ Agora/app/Http/Controllers/UsuarioController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DateTime;
+use App\Models\usuarios;
 
 class UsuarioController extends Controller
 {
-    //
+ public function ListaUsuarios(Request $Request)
+    {
+        $usuario= usuarios::select(
+        'correo',
+        'contrasena',
+        'estado',
+        'fecha_registro',
+        'fecha_actualizacion',
+        'ultimo_acceso',
+        'rol_id',
+        'sesion',
+        'tiempo_uso');
+    return response()->json($usuario);
+    }
+    public function obtenerUsuario(Request $Request, $id)
+    {
+        $usuario= usuarios::where('id', '=', $id)->first();
+        return response()->json();
+    }
+    public function Ingresar_Usuario(Request $Request)
+    {
+        $data=array(
+            'correo'=>$Request->correo,
+            'contrasena'=>$Request->contrasena,
+            'estado'=>$Request->estado,
+            'fecha_registro'=>$Request->fecha_estado,
+            'fecha_actualizacion'=>$Request->fecha_actualizacion,
+            'ultimo_acceso'=>$Request->ultimo_acceso,
+            'rol_id'=>$Request->rol_id,
+            'sesion'=>$Request->sesion,
+            'tiempo_uso'=>$Request->tiempo_uso
+        );
+        $nuevo_usuario= new usuarios($data);
+        $nuevo_usuario->save();
+        return response()->json($nuevo_usuario);
+    }
 }
