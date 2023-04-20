04858686b0459d8c7c58eca6fe6ecb6656f8e2a5
elenuco <elenuco36@gmail.com>
2023-04-20 10:48:27 -0600

    feat(Seagregaron nuevas funciones al controlador): :zap: pendientes los request y otras funciones del controlador
    nuevas funciones en el authenticathor
    
    pendientes funciones de iniciar sesion y tokens

--- Agora/app/Http/Controllers/AuthenticathorController.php
+++ Agora/app/Http/Controllers/AuthenticathorController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\usuarios;
 use Illuminate\Http\Request;
-
+use DateTime;
+use Auth;
+use Carbon\Carbon;
+//Libreria para formatear texto
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+use Illuminte\Support\Facades\Storage;
 class AuthenticathorController extends Controller
 {
-    //
+    public function registro(Request $request){
+        $actual=new DateTime;
+        $imagen=$request->file('foto');
+        $extension=$imagen->extension();
+        $nombreImagen= Str::slug($request->usuario).".".$extension;
+    }
 }
