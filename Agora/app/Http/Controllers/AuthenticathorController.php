089af39d2842db54a8e5f7e40ca391b5ac606744
elenuco <elenuco36@gmail.com>
2023-04-20 14:00:49 -0600

    feat(se agregaron cambios en el Authenticator): :fire: pendiente arreglar la variable en imagen  e integrar con el resto de backend
    pendientes en  request
    
    pendiente los requests

--- Agora/app/Http/Controllers/AuthenticathorController.php
+++ Agora/app/Http/Controllers/AuthenticathorController.php
@@ -18,5 +18,63 @@ class AuthenticathorController extends Controller
         $imagen=$request->file('foto');
         $extension=$imagen->extension();
         $nombreImagen= Str::slug($request->usuario).".".$extension;
+        $data=array(
+            'usuario'=>$request->usuario,
+            'foto_perfil'=>$nombreImagen,
+            'correo'=>$request->correo,
+            'contrasena'=>bcrypt($request->contrasena),
+            'estado'=>1,
+            'fecha_creacion'=>$actual,
+            'fecha_actualizacion'=>$actual,
+        );
+        $imagen->stroreAS('fotos-perfil/',$nombreImagen);
+        $nuevoUsuario= new usuarios($data);
+        $nuevoUsuario->save();
+        $mensaje =array(
+            'mensaje'=>"Usuario registrado exitosamente."
+        );
+        return response()->json($mensaje);
     }
+    public function iniciarSesion(Request $request){
+        $credenciales= request(['correo', 'contrasena']);
+        if (Auth::attempt($credenciales)==false) {
+            $mensaje= array(
+                'mensaje'=>'Verifique sus credenciales'
+            );
+            return response()->json($mensaje, 401);
+        }
+        $usuario=$request->user();
+        $generarToken=$usuario->CreateToken('user Acces Token');
+        $token=$generarToken->token;
+        $token->save();
+        $respuesta=array(
+            'token_acceso'=>$generarToken->accessToken,
+            'tipo_token'=>'Bearer',
+            'fecha_expiracion'=>$generarToken->token->mnexpires_at
+        );
+        return response()->json($respuesta);
+    }
+    public function perfil(Request $request){
+        $informacion = $request->usuario();
+        return response()->json($informacion);
+    }
+    public function cerrarSesion(Request $request){
+        $informacion= $request->user();
+        $informacion->token()->revoke();
+        $mensaje=[
+            'mensaje'=>'Cierre de sesion exitoso'
+        ];
+        return response()->json($mensaje);
+    }
+    public function verFotoPerfil($nombreImagen){
+        $ruta =storage_path('app/fotos-perfil'.$nombreImagen);
+        if (file_exists($ruta)==false) {
+            abort(404);
+        }
+    }
+    $imagen= File::get($ruta);
+    $tipo= File::mimeType($ruta);
+    $respuesta=Response::make($imagen, 200);
+    $respuesta=header("Content-Type", $tipo);
+    return $respuesta;
 }
